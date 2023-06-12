e308408bea7a495077c6e6f1100fe4c84f54580e
Dev Patel <Dev19102611@gmail.com>
2023-06-12 15:06:48 +0530

    add modifile file 1

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from flask import Flask
 # Create a Flask app
 app = Flask(__name__)
 
+# Global variable to store the visit count
+visit_count = 0
+
 # Define a route and the corresponding function
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    global visit_count
+    visit_count += 1
+    return f'Hello, World! This page has been visited {visit_count} times.'
 
 # Run the app if this script is executed directly
 if __name__ == '__main__':
